b41f45ae245c10eeb10b0e05e6e85626fa08de7f
oze.laura <ozelara@gmail.com>
2025-03-31 10:47:26 +0200

    Add project files.

---
+++ ICityMethods.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConPop
+{
+    interface ICityMethods
+    {
+        /// <summary>
+        /// Betölti a városok adatait CSV fájlból
+        /// </summary>
+        /// <param name="path">A megadott útvonalon lévő fájlból olvas be</param>
+        void LoadFromCSV(string path);
+
+        /// <summary>
+        /// Elmenti a városok adatait CSV fájlba
+        /// </summary>
+        /// <param name="path">A megadott útvonalon lévő fájlba írja ki az adatokat</param>
+        /// <param name="cities">A városok listája</param>
+        /// <param name="charCode">A karakterkódolást határozzsa meg</param>
+        void SaveToCSV(string path, List<City> cities, string charCode="UTF-8");
+
+        /// <summary>
+        /// Visszaadja a beolvasott városok darabszámát
+        /// </summary>
+        int CityCount();
+
+        /// <summary>
+        /// Folyamatosan növekvő-e a megadott népességi adatok sorozata?
+        /// </summary>
+        /// <returns>true esetén igen</returns>
+        bool IsContinuousGrowing(List<int> populationDatas);
+
+        /// <summary>
+        /// Visszaadja a 10 legnagyobb népességű várost a megadott évben
+        /// </summary>
+        List<City> Top10City(int year);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConPop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //A kapott CSV állomány Európa nagyobb városainak a korábbi és a jövőbeli becsült népességszámait tartalmazza.
+            // CITY_CODE;CITY_NAME;Y2010;Y2020;Y2030;Y2040;Y2050
+
+
+            //todo: 1F Készítsen osztályt egy város adatainak tárolására! (City)
+            //todo: 2F Készítsen (CityDataProvider) néven osztályt a városok adatainak kezelésére, ami a megadott (CitiesMethods) interfészt implementálja!
+
+            //todo: Válaszoljon a következő kérdésekre a korábban létrehozott osztályok felhasználásával!
+
+            //todo: 3F Hány város adatait tartalmazza a CSV fájl? (CityCount)
+            //todo: 4F Melyik 10 város volt a legnépesebb 2020-ban? (Top10City)
+            //todo: 5F Kérje be billentyűzetről egy város nevét! Ha nem létezik, akkor jelezze azt és kérje be újra! 
+            //         Miután létező nevet adott meg, döntse el, hogy a város lakossága folyamatosan növekedett-e az évek alatt? (IsContinuousGrowing)
+            //todo: 6F Írja (bigCities.CSV) fájlba a 2020-ban 1 millió főnél nagyobb népességgel rendelkező városokat! (SaveToCSV)
+            //todo: 7F Írassa képernyőre azoknak a városoknak a nevét és népességváltozását, ahol 2050-ben kevesebben lesznek mint 2010-ben voltak!
+            //todo:    A kiíratás népességcsökkenés szerint növevően rendezetten történjen! 
+            //todo:    Tipp: Érdemes megfelelő metódussal vagy property-vel bővíteni az osztályt! (CityDataProvider)
+        }
+    }
+}
